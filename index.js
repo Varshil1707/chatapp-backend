5f3c16ea3acd171e5715c5d08c7a95a877e141a4
Varshil <varshil.shah@ecosmob.com>
2023-01-18 17:23:08 +0530

    jan 18

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 const http = require("http");
 const express = require("express");
-const authRoutes = require("./authentication");
-
+// const authRoutes = require("./authentication");
 const cors = require("cors");
 const socketIo = require("socket.io");
 require("dotenv").config();
-const port = process.env.PORT ;
+const port = process.env.PORT;
 const app = express();
 const server = http.createServer(app);
 app.use(express.json());
@@ -21,23 +20,22 @@ let message = "left";
 
 io.on("connection", (socket) => {
   console.log("New Connection");
-  socket.on("disconnect", () => {
-    console.log("disconnected");
-    socket.broadcast.emit("disconnect-method", message);
-  });
-
-  
+  socket.emit("clientsCount", {count : io.engine.clientsCount})
+  console.log("Line 26",io.engine.clientsCount)
   socket.on("joined", ({ user }) => {
     users[socket.id] = user;
     console.log(`${user} is  joined`);
-    socket.emit("userJoined", {
+    socket.broadcast.emit("userJoined", {
       user: "Admin",
       message: `Welcome to the chat ${user}`,
     });
-    
-    socket.emit("welcome", { user: "Admin", message: `Welcome to the chat ${user} ` });
+
+    socket.emit("welcome", {
+      user: "Admin",
+      message: `Welcome to the chat ${user} `,
+    });
   });
-  
+
   socket.on("message", ({ message, id }) => {
     io.emit("sentMessage", { user: users[id], message: message, id });
   });
@@ -47,7 +45,7 @@ if (process.env.PORT == "production") {
   app.use(express.static("chat/build"));
 }
 
-app.use("/", authRoutes);
+// app.use("/", authRoutes);
 
 server.listen(port, () => {
   console.log(`server is working on http://localhost:${port}`);
